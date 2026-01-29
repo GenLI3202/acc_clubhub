1bc57a8a9727d5a91067fe3df6cd4aa148438b83
Hiligen723 <gen.li@tum.de>
2026-01-29 23:38:36 +0100

    change bg picture

--- frontend/.astro/content-assets.mjs
+++ frontend/.astro/content-assets.mjs
@@ -1,5 +1,6 @@
 
 import __ASTRO_IMAGE_IMPORT_1TD42Y from "image/alps-summer-2025/1769721356622.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fmedia%2Fzh%2Falps-summer-2025.md";
 import __ASTRO_IMAGE_IMPORT_Z1F3WtN from "image/alps-summer-2025/1769721370426.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fmedia%2Fzh%2Falps-summer-2025.md";
-export default new Map([["image/alps-summer-2025/1769721356622.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fmedia%2Fzh%2Falps-summer-2025.md", __ASTRO_IMAGE_IMPORT_1TD42Y], ["image/alps-summer-2025/1769721370426.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fmedia%2Fzh%2Falps-summer-2025.md", __ASTRO_IMAGE_IMPORT_Z1F3WtN]]);
+import __ASTRO_IMAGE_IMPORT_1aSqEL from "image/alps-summer-2025/1769725238921.png?astroContentImageFlag=&importer=src%2Fcontent%2Fmedia%2Fzh%2Falps-summer-2025.md";
+export default new Map([["image/alps-summer-2025/1769721356622.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fmedia%2Fzh%2Falps-summer-2025.md", __ASTRO_IMAGE_IMPORT_1TD42Y], ["image/alps-summer-2025/1769721370426.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fmedia%2Fzh%2Falps-summer-2025.md", __ASTRO_IMAGE_IMPORT_Z1F3WtN], ["image/alps-summer-2025/1769725238921.png?astroContentImageFlag=&importer=src%2Fcontent%2Fmedia%2Fzh%2Falps-summer-2025.md", __ASTRO_IMAGE_IMPORT_1aSqEL]]);
 		
